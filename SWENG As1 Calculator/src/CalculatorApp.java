58a0fd2a932216b448de04bf84c9d5af69a986d8
albogacs <Sultanalbo06@gmail.com>
2022-10-06 22:30:32 +0100

    unit testing done

--- SWENG As1 Calculator/src/CalculatorApp.java
+++ SWENG As1 Calculator/src/CalculatorApp.java
@@ -154,62 +154,6 @@ public class CalculatorApp {
                             " strings that contain integers and operands +, - or *.\n" +
                             "For example, 432+89*3-56");
         }
-
-
-
-
-
-//        // The tokens that make up the input
-//        String[] tokens = userCalculations.split("");
-//
-//        String[] operators = userCalculations.split("[0-9]");
-//        String[] operands = userCalculations.split("[*+-]");
-//        System.out.print((Arrays.toString(operands) + "and" + Arrays.toString(operators)));
-//
-//        int aggregate = 0;
-//        String operator = "";
-//
-//        for (int i = 0; i <= tokens.length -1; i++) {
-//
-//            switch (tokens[1].charAt(0)) {
-//
-//                // case to add two numbers
-//                case '*':
-//
-//                    aggregate *= (Integer.parseInt(tokens[0])) * (Integer.parseInt(tokens[2]));
-//
-//                    break;
-//
-//                // case to subtract two numbers
-//                case '-':
-//                    aggregate -= Integer.parseInt(tokens[0])
-//                            - Integer.parseInt(tokens[2]);
-//
-//                    break;
-//
-//                // case to multiply two numbers
-//                case '+':
-//                    aggregate += Integer.parseInt(tokens[0])
-//                            + Integer.parseInt(tokens[2]);
-//
-//                    break;
-//
-//                default:
-//
-//                    System.out.println("You enter wrong input");
-//
-//                    break;
-//            }
-//        }
-//
-//
-//            System.out.println("The final result:");
-//
-//            System.out.println();
-//
-//            // print the final result
-//            System.out.print(aggregate);
-
     }
 }
 
